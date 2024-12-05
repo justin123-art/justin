26a1fc443f2ccd5a6142792c90663d75c9768ea5
justin123-art <mcatanoy316@gmail.com>
2024-12-05 10:26:07 +0800

    12

---
+++ page/authentication.php
@@ -0,0 +1,132 @@
+<?php
+	//import model
+	include_once '../model/authenticationModel.php';
+
+	$page_info['page'] = 'login'; //for page that needs to be called
+	$page_info['sub_page'] = isset($_GET['sub_page'])? $_GET['sub_page'] : 'login'; //for function to be loaded
+		
+	//-----------------------//
+	//--     --//
+	//-----------------------//
+	session_start();
+	if(!isset($_SESSION['loggedin'])){
+		try {//used try to catch unfortunate errors
+			//check for active function
+			if (isset($_GET['function'])){
+				new LoginActive($page_info);
+			}else{
+				//no active function, use the default page to view
+				new Login($page_info);
+			}		
+			
+		}catch (Throwable $e){ //get the encountered error
+			echo '<h1>ERROR 404</h1>';
+			echo $e->getMessage();
+		}//end of validation
+	}else{
+		header("Location: ../page/home.php");
+	}
+	
+	
+	
+	//-----------------------//
+	//--  Class Navigation --//
+	//-----------------------//
+	class Login{
+		//set default page info
+		private $page = '';
+		private $sub_page = '';
+		
+		//run function construct
+		function __construct($page_info){
+			//assign page info
+			$this->page = $page_info['page'];
+			$this->sub_page = $page_info['sub_page'];
+			
+			//run the function
+			$this->{$page_info['sub_page']}();
+		}
+		
+		//-----------------------------//
+		//--   function start here   --//
+		function login(){
+			include '../views/login.php';
+		}
+		function register(){
+			include '../views/register.php';
+		}
+	}
+	
+	//-----------------------//
+	//-- Active Class      --//
+	//-----------------------//
+	class LoginActive{
+		//set default page info
+		private $page = '';
+		private $sub_page = '';
+		
+		//run function construct
+		function __construct($page_info){
+			//assign page info
+			$this->page = $page_info['page'];
+			$this->sub_page = $page_info['sub_page'];
+			
+			//run the function
+			$this->{$page_info['sub_page']}();
+		}
+		
+		//-----------------------------//
+		//--   function start here   --//
+		function login(){
+			//stanciation
+			$authentication = new authenticationModel();
+			
+			
+			//authenticat login
+			$login = $authentication->authenticationLogin($_POST);
+			
+			
+			//check if login success
+			if ($login){
+				if($login['user_type'] == 'admin'){
+
+					header('location:../page/admin.php');
+				}else{
+					$_SESSION['loggedin'] = 'active';
+					header('location:../page/home.php');
+				}
+			}else{
+				$msg = "Invalid Username or Password!";
+				include '../views/login.php';
+			}
+		}
+		function register(){//register user
+		
+			//instantiate class model
+			$authentication_register = new authenticationModel();
+			
+			//check for correct password
+			if ($_POST['password'] == $_POST['confirm_password']){
+				//if correct password, process register
+				//prepare temp array
+				$temp_array = [];
+				$temp_array['name'] = $_POST['name'];
+				$temp_array['email'] = $_POST['email'];
+				$temp_array['password'] = $_POST['password'];
+				
+				//process registration
+				$register = $authentication_register->register($_POST);
+				
+				echo "<script>alert('Successfully registered.');</script>";
+				include "../views/login.php";
+				
+			}else{
+				//error message
+				$msg = 'Invalid Password!';
+				//back
+				include "../views/register.php";
+			}
+			
+		}
+	}
+?>
