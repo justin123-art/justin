06f7aa75fc1b81bd758bee15468c26d2f91ce5a0
justin123-art <mcatanoy316@gmail.com>
2024-12-05 11:37:07 +0800

    22

--- views/home.php
+++ views/home.php
@@ -1,11 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+include 'nav/header.php';
+?>
+ 
+ <!-- home content -->
+    <section class="home">
+        <div class="content">
+            <h5>Hello</h5>
+            <h3>I'm <span>Shahbaz</span></h3>
+            <h2 class="changecontent"></h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum
+                <br>Lorem ipsum dolor sit amet.
+            </p>
+            <button id="btn1">HIRE ME</button>
+        </div>
+        <div class="img">
+            <img src="./image/me.png" alt="">
+        </div>
+    </section>
+    <!-- home content -->
+
+
+    </div>
+    <!-- home section -->
