77d887bae67c7e010256f06293ea629bb623c5c8
VOLTES_V <paradoashlee@gmail.com>
2024-12-11 15:44:51 +0800

    new

--- views/about.php
+++ views/about.php
@@ -11,27 +11,13 @@ include 'nav/header.php';
       <img src="../assets/img/about.jpg" class="img-fluid" alt="">
     </div>
     <div class="col-lg-6 order-2 order-lg-1 content">
-      <h3>About Us</h3>
-      <p class="fst-italic">
-      Welcome to Car Wash , where we believe that every vehicle deserves a little TLC! Founded in 2015, our mission is to provide exceptional car wash and detailing services that leave your vehicle looking pristine and feeling fresh.
-      We’re not just washing cars; we’re building relationships. We invite you to experience the difference and see why so many customers trust us with their vehicles. Thank you for choosing us as your go-to car wash!
-      </p>
-    </div>
-    <div class="col-lg-6 order-2 order-lg-1 content">
-      <h3>Our Services</h3>
-      <p class="fst-italic">
-      We offer a range of services tailored to meet your needs, including:
-      </p>
-      <ul>
-        <li><i class="bi bi-check2-all"></i> <span>SHINY TIRES : We use a superior water-based silicone product that delivers a glistening shine while reducing any sling that ends up on the vehicle.</span></li>
-        <li><i class="bi bi-check2-all"></i> <span>GO WAX : Go Wax is made up of special formulated polymers.These fill the microspic pores in the cars paints and glass to provide advance 
-								protection and long-lasting crystal-like shine.</span></li>
-        <li><i class="bi bi-check2-all"></i> <span>PRIME DRY : A low ph foam that helps prep the surface of the vehicle to aid in the beading and grouping of water molecules to promote
-        water repellency    and help produce an extremely dry car.</span></li></ul>
-      <p>
-      We’re not just washing cars; we’re building relationships. We invite you to experience the difference and see why so many customers trust us with their vehicles. Thank you for choosing us as your go-to car wash!
+      <h3 class="m-0 mt-4 text-body-emphasis text-lg leading-6 fw-bold about-title">
+          ABOUT US
+      </h3>
 
-
+      <p class="fst-italic">
+        Welcome to Car Wash, where we believe that every vehicle deserves a little TLC! Founded in 2015, our mission is to provide exceptional car wash and detailing services that leave your vehicle looking pristine and feeling fresh.
+        We’re not just washing cars; we’re building relationships. We invite you to experience the difference and see why so many customers trust us with their vehicles. Thank you for choosing us as your go-to car wash!
       </p>
     </div>
   </div>
@@ -39,3 +25,10 @@ include 'nav/header.php';
 </div>
 
 </section><!-- /About Section -->
+
+<style>
+  .about-title {
+    font-size: 36px;  /* Larger font size */
+    font-weight: bolder;  /* Bolder text */
+  }
+</style>
